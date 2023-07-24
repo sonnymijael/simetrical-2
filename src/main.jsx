feb0a9cbc2848a603f4dbec658404b0cb64499ab
Sonny Mijael <sonny.arce@kiubix.com>
2023-07-24 15:40:07 -0600

    Remove comments

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 import { ToastContainer } from 'react-toastify'
 // CSS styles
 import './index.css'
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
